3fc9db913fe04c6890ff1ea02e76b687b358506c
hamzahpane <m.hamzahpane27@gmail.com>
2024-07-22 13:37:08 +0700

    penambahan varcel

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { dbHost, dbPort, dbName } = require('../app/config');
 
-mongoose.connect(`mongodb://${dbHost}:${dbPort}/${dbName}`, {
+mongoose.connect(`mongodb+srv://mhamzahpane27:jOQlxYYKvSnlrKVh@cluster0.5mgvauf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
 useNewUrlParser: true
 });
 const db = mongoose.connection;
